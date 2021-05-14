8d254f0744c56ec45db8b8767e6baf821511a02e
Chris <tulip.chris@gmail.com>
2021-05-14 17:51:44 -0400

    Insult and Cast Member base structure

--- TOGameJam2021-AiaB/Assets/Scripts/CastMember.cs
+++ TOGameJam2021-AiaB/Assets/Scripts/CastMember.cs
@@ -2,17 +2,83 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum InsultArmorType
+{
+    NONE,
+    RED,
+    YELLOW,
+    GREEN,
+    NUM_ARMOR_TYPES
+}
+
+
 public class CastMember : MonoBehaviour
 {
+    float m_fConfidence = 10.0f;
+    [SerializeField]
+    InsultArmorType m_eArmor;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (m_eArmor == 0)
+        {
+            m_eArmor = (InsultArmorType)Random.Range(1, 3);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void TakeDamage(float damagevalue, InsultDamageType damagetype)
+    {
+        switch(damagetype)
+        {
+            case InsultDamageType.RED:
+                if(m_eArmor == InsultArmorType.RED)
+                {
+                    Debug.Log("RED ON RED");
+                } else if(m_eArmor == InsultArmorType.YELLOW)
+                {
+                    Debug.Log("RED ON YELLOW");
+                } else if (m_eArmor == InsultArmorType.GREEN)
+                {
+                    Debug.Log("RED ON GREEN");
+                }
+                    break;
+            case InsultDamageType.YELLOW:
+                if (m_eArmor == InsultArmorType.RED)
+                {
+                    Debug.Log("YELLOW ON RED");
+                }
+                else if (m_eArmor == InsultArmorType.YELLOW)
+                {
+                    Debug.Log("YELLOW ON YELLOW");
+                }
+                else if (m_eArmor == InsultArmorType.GREEN)
+                {
+                    Debug.Log("YELLOW ON GREEN");
+                }
+                break;
+            case InsultDamageType.GREEN:
+                if (m_eArmor == InsultArmorType.RED)
+                {
+                    Debug.Log("GREEN ON RED");
+                }
+                else if (m_eArmor == InsultArmorType.YELLOW)
+                {
+                    Debug.Log("GREEN ON YELLOW");
+                }
+                else if (m_eArmor == InsultArmorType.GREEN)
+                {
+                    Debug.Log("GREEN ON GREEN");
+                }
+                break;
+            default:
+                break;
+        }
     }
 }
--- TOGameJam2021-AiaB/Assets/Scripts/Insult.cs
+++ TOGameJam2021-AiaB/Assets/Scripts/Insult.cs
@@ -2,17 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum InsultDamageType
+{
+    NONE,
+    RED,
+    YELLOW,
+    GREEN,
+    NUM_DAMAGE_TYPES
+}
+
 public class Insult : MonoBehaviour
 {
+    float m_fInsultDamage = 5.0f;
+    [SerializeField]
+    InsultDamageType m_eDamageType = 0;
+    [SerializeField]
+    CastMember m_gAssaultedActor;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(m_eDamageType == 0)
+        {
+            m_eDamageType = (InsultDamageType)Random.Range(1, 3);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnMouseDown()
+    {
+        //m_gAssualtedActor = GameManger.Instance.GetCurrentActor()
+        m_gAssaultedActor.TakeDamage(m_fInsultDamage, m_eDamageType);
+        Destroy(this.gameObject);
     }
 }
